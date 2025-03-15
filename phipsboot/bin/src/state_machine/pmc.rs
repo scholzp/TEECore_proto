5295668a3ad8bbf16d2e035b8115fac70d450bd1
Pascal Scholz <pascal.scholz@cyberus-technology.de>
2025-03-15 12:04:10 +0100

    Add 4 additional PMCs and more events

--- phipsboot/bin/src/state_machine/pmc.rs
+++ phipsboot/bin/src/state_machine/pmc.rs
@@ -4,80 +4,93 @@ use lib::pmc_utils::vendor;
 use lib::pmc_utils::intel;
 use lib::pmc_utils::architectural;
 
-pub fn setup_pmcs() {
-    use vendor::{check_vendor, CpuVendor};
+const COUNTER_NUM: usize = 3;
+const COUNTER_NUM_P: usize = 3 + 1;
 
-    if false == check_vendor(CpuVendor::Intel) {
-        return;
-    }
+pub fn setup_pmcs() {
+	use vendor::{check_vendor, CpuVendor};
+
+	if false == check_vendor(CpuVendor::Intel) {
+		return;
+	}
 	setup_architecturial();
-    // setup_offcore();
+	setup_offcore();
 }
 
 fn setup_offcore() {
-    use intel::MsrOffcoreRspEventCounter;
+	use intel::MsrOffcoreRspEventCounter;
 
-    let mut counter = MsrOffcoreRspEventCounter::new(0, 0);
-    counter.set_offcore_configuration(
-        // 0x0_u64 | intel::SUPPLIER_ANY
-        // | intel::REQUEST_DMND_DATA_RD
-        // | intel::REQUEST_DMND_RFO
-        // | intel::REQUEST_DMND_IFETCH
-        // | intel::REQUEST_DMND_HWPF_L2_DATA_RD
-        // | intel::REQUEST_DMND_HWPF_L2_RFO
-        // | intel::REQUEST_DMND_HWPF_L3
-        // | intel::REQUEST_DMND_HWPF_L1D_AND_SWPF
-        // | intel::REQUEST_DMND_STREAMING_WR
-        // | intel::REQUEST_OTHER
-        0x184000001_u64
-    );
-    counter.activate_counter(0x0_u64);
+	let mut counter = MsrOffcoreRspEventCounter::new(0, 3);
+	counter.set_offcore_configuration(
+		// 0x0_u64 | intel::SUPPLIER_ANY
+		// | intel::REQUEST_DMND_DATA_RD
+		// | intel::REQUEST_DMND_RFO
+		// | intel::REQUEST_DMND_CODE_RD
+		// | intel::REQUEST_DMND_HWPF_L2_DATA_RD
+		// | intel::REQUEST_DMND_HWPF_L2_RFO
+		// | intel::REQUEST_DMND_HWPF_L3
+		// | intel::REQUEST_DMND_HWPF_L1D_AND_SWPF
+		// | intel::REQUEST_DMND_STREAMING_WR
+		// | intel::REQUEST_OTHER
+		// 0x184000001_u64
+        // 0x0_u64 | (0x1 << 18) | (0x1 << 19) | (0x1 << 20) // Spulier = L3
+        // | (0x1 << 34) | (0x1 << 36) // SNOOP_HITM & SNOOP_HIT_NO_FW
+        // | (0x1 << 0) | (0x1 << 1) | (0x1 << 2) // DATA_RD, RFO, CODE_RD
+        0x10800_u64
+	);
+	counter.activate_counter(0x0_u64);
 }
 
 fn setup_architecturial() {
 	use architectural::{
 		ArchitecturalEventCounter, EVENT_SKYLAKE_L2_LINES_IN_ALL,
 		EVENT_PREDEFINED_LLC_REFERENCES, EVENT_SKYLAKE_L2_REQUEST_MISS,
-        EVENT_ICELAKE_OFFCORE_ALL_EVENTS,
-        EVENT_ICELAKE_MEM_LOAD_MISC_RETIRED_UC,
-        EVENT_ICELAKE_MEM_LOAD_L3_HIT_RETIRED_XSNP_HITM,
-        EVENT_ICELAKE_L2_ALL_DEMAND_MISS,
-        EVENT_ICELAKE_L2_ALL_DEMAND_DATA_RD,
+		EVENT_ICELAKE_OFFCORE_ALL_REQUESTS,
+		EVENT_ICELAKE_MEM_LOAD_MISC_RETIRED_UC,
+		EVENT_ICELAKE_MEM_LOAD_L3_HIT_RETIRED_XSNP_HITM,
+		EVENT_ICELAKE_L2_ALL_DEMAND_MISS,
+		EVENT_ICELAKE_L2_ALL_DEMAND_DATA_RD,
+		EVENT_ICELAKE_MEM_LOAD_RETIRED_L1_HIT,
+		EVENT_ICELAKE_L1D_REPLACEMENT,
+		EVENT_ICELAKE_MEM_LOAD_RETIRED_L1_MISS_ANY,
+		EVENT_ICELAKE_MEM_LOAD_RETIRED_L1_MISS,
 		IA32_PERFEVTSEL_OS, IA32_PERFEVTSEL_USR
 	};
 
-    let mut pmc_0 = ArchitecturalEventCounter::new(0);
-	let mut pmc_1 = ArchitecturalEventCounter::new(1);
-	let mut pmc_2 = ArchitecturalEventCounter::new(2);
-	let mut pmc_3 = ArchitecturalEventCounter::new(3);
+    let mut counters: [ArchitecturalEventCounter; COUNTER_NUM] = [ArchitecturalEventCounter::new(0); COUNTER_NUM];
+    for x in 0..COUNTER_NUM {
+        counters[x].set_index(x as u8);
+    }
 
-    pmc_0.set_configuration(EVENT_ICELAKE_MEM_LOAD_MISC_RETIRED_UC | IA32_PERFEVTSEL_OS | IA32_PERFEVTSEL_USR);
-	pmc_1.set_configuration(EVENT_ICELAKE_OFFCORE_ALL_EVENTS | IA32_PERFEVTSEL_OS | IA32_PERFEVTSEL_USR);
-	pmc_2.set_configuration(EVENT_ICELAKE_L2_ALL_DEMAND_DATA_RD | IA32_PERFEVTSEL_OS | IA32_PERFEVTSEL_USR);
-	pmc_3.set_configuration(EVENT_ICELAKE_L2_ALL_DEMAND_MISS | IA32_PERFEVTSEL_OS | IA32_PERFEVTSEL_USR);
+	counters[0].set_configuration(EVENT_ICELAKE_L1D_REPLACEMENT | IA32_PERFEVTSEL_OS | IA32_PERFEVTSEL_USR);
+	counters[1].set_configuration(EVENT_ICELAKE_MEM_LOAD_RETIRED_L1_HIT | IA32_PERFEVTSEL_OS | IA32_PERFEVTSEL_USR);
+	counters[2].set_configuration(EVENT_ICELAKE_OFFCORE_ALL_REQUESTS | IA32_PERFEVTSEL_OS | IA32_PERFEVTSEL_USR);
+	// counters[3].set_configuration(EVENT_ICELAKE_MEM_LOAD_RETIRED_L1_MISS_ANY | IA32_PERFEVTSEL_OS | IA32_PERFEVTSEL_USR);
 
-    pmc_0.activate_counter(0);
-	pmc_1.activate_counter(0);
-	pmc_2.activate_counter(0);
-	pmc_3.activate_counter(0);
+    for x in 0..COUNTER_NUM {
+        counters[x].activate_counter(0);
+    }
 }
 
 
 pub fn read_and_print_pmcs() {
 	use architectural::{ArchitecturalEventCounter};
-    use vendor::{check_vendor, CpuVendor};
+	use vendor::{check_vendor, CpuVendor};
 
-    if false == check_vendor(CpuVendor::Intel) {
-        return;
+	if false == check_vendor(CpuVendor::Intel) {
+		return;
+	}
+
+    let mut counters: [ArchitecturalEventCounter; COUNTER_NUM_P] = [ArchitecturalEventCounter::new(0); COUNTER_NUM_P];
+    for x in 0..COUNTER_NUM_P {
+        counters[x].set_index(x as u8);
     }
 
-	let pmc_0 = ArchitecturalEventCounter::new(0);
-	let pmc_1 = ArchitecturalEventCounter::new(1);
-	let pmc_2 = ArchitecturalEventCounter::new(2);
-	let pmc_3 = ArchitecturalEventCounter::new(3);
-
-	info!("IA_PMC1 (UC LOADS)   = {:#016x?}", pmc_0.read_pcm_val());
-	info!("IA_PMC0 (ALL OFC)    = {:#016x?}", pmc_1.read_pcm_val());
-	info!("IA_PMC2 (L2 ALL DRD) = {:#016x?}", pmc_2.read_pcm_val());
-	info!("IA_PMC3 (L2 ALL MSS) = {:#016x?}", pmc_3.read_pcm_val());
+	info!("IA_PMC1 (Replacement) = {:#016x?}", counters[0].read_pcm_val());
+	info!("IA_PMC0 (L1 Hit)      = {:#016x?}", counters[1].read_pcm_val());
+	info!("IA_PMC2 (All OffCore) = {:#016x?}", counters[2].read_pcm_val());
+	info!("IA_PMC3 (Own OffCore) = {:#016x?}", counters[3].read_pcm_val());
+    // info!("IA_PMC4 (UC MEM Accs) = {:#016x?}", counters[4].read_pcm_val());
+	// info!("IA_PMC5 (L3 HitM Snp) = {:#016x?}", counters[5].read_pcm_val());
+	// info!("IA_PMC6 (L2 ALL Miss) = {:#016x?}", counters[6].read_pcm_val());
 }
