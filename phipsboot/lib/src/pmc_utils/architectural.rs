cf38304edf824df718754162e240b2eb0052be68
Pascal Scholz <pascal.scholz@cyberus-technology.de>
2025-03-15 12:02:40 +0100

    Add support for 8 PMCs

--- phipsboot/lib/src/pmc_utils/architectural.rs
+++ phipsboot/lib/src/pmc_utils/architectural.rs
@@ -1,9 +1,13 @@
 use x86::msr::{
 	rdmsr, wrmsr,
-	IA32_PERFEVTSEL0, IA32_A_PMC0,
-	IA32_PERFEVTSEL1, IA32_A_PMC1,
-	IA32_PERFEVTSEL2, IA32_A_PMC2,
-	IA32_PERFEVTSEL3, IA32_A_PMC3,
+	IA32_PERFEVTSEL0, IA32_PMC0,
+	IA32_PERFEVTSEL1, IA32_PMC1,
+	IA32_PERFEVTSEL2, IA32_PMC2,
+	IA32_PERFEVTSEL3, IA32_PMC3,
+	IA32_PERFEVTSEL4, IA32_PMC4,
+	IA32_PERFEVTSEL5, IA32_PMC5,
+	IA32_PERFEVTSEL6, IA32_PMC6,
+	IA32_PERFEVTSEL7, IA32_PMC7,
 };
 
 use log::info;
@@ -43,18 +47,21 @@ pub const EVENT_SKYLAKE_L2_LINES_IN_ALL: u64 = 0xf1_u64 | 0x1f_u64 << 8;
 /// Predefined events that counts references to on-die LLC
 pub const EVENT_PREDEFINED_LLC_REFERENCES: u64 = 0x2e_u64 | 0x4f_u64 << 8;
 
-pub const EVENT_ICELAKE_OFFCORE_ALL_EVENTS: u64 = 0x21_u64 | 0x80_u64 << 8;
+pub const EVENT_ICELAKE_OFFCORE_ALL_REQUESTS: u64 = 0x21_u64 | 0x80_u64 << 8;
 pub const EVENT_ICELAKE_MEM_LOAD_MISC_RETIRED_UC: u64 = 0xd4_u64 | 0x04_u64 << 8;
 pub const EVENT_ICELAKE_MEM_LOAD_L3_HIT_RETIRED_XSNP_HITM: u64 = 0xd2_u64 | 0x04_u64 << 8;
 pub const EVENT_ICELAKE_L2_ALL_DEMAND_DATA_RD: u64 = 0x24_u64 | 0xe1_u64 << 8;
-// pub const EVENT_ICELAKE_L2_ALL_DEMAND_MISS: u64 = 0x24_u64 | 0x21_u64 << 8;
-
+pub const EVENT_ICELAKE_L2_ALL_DEMAND_MISS: u64 = 0x24_u64 | 0x27_u64 << 8;
+pub const EVENT_ICELAKE_MEM_LOAD_RETIRED_L1_HIT: u64 = 0xd1_u64 | 0x01_u64 << 8;
+pub const EVENT_ICELAKE_MEM_LOAD_RETIRED_L1_MISS: u64 = 0xd1_u64 | 0x08_u64 << 8;
+pub const EVENT_ICELAKE_MEM_LOAD_RETIRED_L1_MISS_ANY: u64 = 0x43_u64 | 0xfd_u64 << 8;
+pub const EVENT_ICELAKE_L1D_REPLACEMENT: u64 = 0x51_u64 | 0x01_u64 << 8;
 // Retired all stores: pub const EVENT_ICELAKE_L2_ALL_DEMAND_MISS: u64 = 0xd0_u64 | 0x82_u64 << 8;
 // All L1 hits pub const EVENT_ICELAKE_L2_ALL_DEMAND_MISS: u64 = 0xd1_u64 | 0x01_u64 << 8;
 // L2 Hits as source
-pub const EVENT_ICELAKE_L2_ALL_DEMAND_MISS: u64 = 0xd1_u64 | 0x02_u64 << 8;
+// pub const EVENT_ICELAKE_L2_ALL_DEMAND_MISS: u64 = 0xd1_u64 | 0x02_u64 << 8;
 
-
+#[derive(Copy, Debug, Clone)]
 pub struct ArchitecturalEventCounter {
 	pmc_index: u8,
 	event_config: u64,
@@ -113,54 +120,61 @@ impl ArchitecturalEventCounter {
 		*  1) Stop IA32_PMCx.
 		*  2) Configure the IA32_PERFEVTSELx with the behavior we wish for
 		*  3) Event and UMASK in IA32_PERFEVTSELx are chosen so that the
-		*     PMC uses the configuration from MSR_OFFCORE_RSPx
+		*	 PMC uses the configuration from MSR_OFFCORE_RSPx
 		*  4) Initialize IA32_PMCx (do we increment, do we decrement...?)
 		*  5) Start the counter by setting the bit in IA32_PERFEVTSELx
 		*/
+		let mut msr_pmc: u32 = 0;
+		let mut msr_pmc_eventsel: u32 = 0;
 		match self.pmc_index {
 			0 => {
-				// We operate on PMC0 and IA32_PERFEVTSEL0
-				Self::init_and_conf_pmc(
-					IA32_PERFEVTSEL0,
-					IA32_A_PMC0,
-					init_v,
-					self.event_config
-				);
+				msr_pmc = IA32_PMC0;
+				msr_pmc_eventsel = IA32_PERFEVTSEL0;
 			},
 			1 => {
 				// We operate on PMC1 and IA32_PERFEVTSEL1
-				Self::init_and_conf_pmc(
-					IA32_PERFEVTSEL1,
-					IA32_A_PMC1,
-					init_v,
-					self.event_config
-				);
+				msr_pmc = IA32_PMC1;
+				msr_pmc_eventsel = IA32_PERFEVTSEL1;
 			},
 			2 => {
 				// We operate on PMC2 and IA32_PERFEVTSEL2
-				Self::init_and_conf_pmc(
-					IA32_PERFEVTSEL2,
-					IA32_A_PMC2,
-					init_v,
-					self.event_config
-				);
+				msr_pmc = IA32_PMC2;
+				msr_pmc_eventsel = IA32_PERFEVTSEL2;
 
 			},
 			3 => {
 				// We operate on PMC3 and IA32_PERFEVTSEL3
-				Self::init_and_conf_pmc(
-					IA32_PERFEVTSEL3,
-					IA32_A_PMC3,
-					init_v,
-					self.event_config
-				);
-
+				msr_pmc = IA32_PMC3;
+				msr_pmc_eventsel = IA32_PERFEVTSEL3;
+			},
+			4 => {
+				// We operate on PMC3 and IA32_PERFEVTSEL3
+				msr_pmc = IA32_PMC4;
+				msr_pmc_eventsel = IA32_PERFEVTSEL4;
+			},
+			5 => {
+				// We operate on PMC3 and IA32_PERFEVTSEL3
+				msr_pmc = IA32_PMC5;
+				msr_pmc_eventsel = IA32_PERFEVTSEL5;
+			},
+			6 => {
+				// We operate on PMC3 and IA32_PERFEVTSEL3
+				msr_pmc = IA32_PMC6;
+				msr_pmc_eventsel = IA32_PERFEVTSEL6;
+			},
+			7 => {
+				// We operate on PMC3 and IA32_PERFEVTSEL3
+				msr_pmc = IA32_PMC7;
+				msr_pmc_eventsel = IA32_PERFEVTSEL7;
 			},
 			_ => {
-				info!("No CPU known to implement 4 or more GP PMCs!");
+				info!("No CPU known to implement 8 or more GP PMCs!");
 				return;  //TODO: We want, at some point, return an error
 			},
 		}
+		Self::init_and_conf_pmc(
+			msr_pmc_eventsel, msr_pmc, init_v, self.event_config
+		);
 	}
 
 	fn init_and_conf_pmc(perfevtsel_register: u32, pmc_register: u32, init_v: u64, perfsel_content: u64) {
@@ -171,18 +185,22 @@ impl ArchitecturalEventCounter {
 			wrmsr(pmc_register, init_v);
 			// MSR_OFFCOREx was configured before
 			// Activate the counter
-			wrmsr(perfevtsel_register, perfsel_content | IA32_PERFEVTSEL_EN);
+			wrmsr(perfevtsel_register, perfsel_content | IA32_PERFEVTSEL_EN | IA32_PERFEVTSEL_E);
 		}
 	}
 
 	pub fn read_pcm_val(&self) -> u64 {
 		match self.pmc_index {
-			0 => unsafe { rdmsr(IA32_A_PMC0) },
-			1 => unsafe { rdmsr(IA32_A_PMC1) },
-			2 => unsafe { rdmsr(IA32_A_PMC2) },
-			3 => unsafe { rdmsr(IA32_A_PMC3) },
+			0 => unsafe { rdmsr(IA32_PMC0) },
+			1 => unsafe { rdmsr(IA32_PMC1) },
+			2 => unsafe { rdmsr(IA32_PMC2) },
+			3 => unsafe { rdmsr(IA32_PMC3) },
+			4 => unsafe { rdmsr(IA32_PMC4) },
+			5 => unsafe { rdmsr(IA32_PMC5) },
+			6 => unsafe { rdmsr(IA32_PMC6) },
+			7 => unsafe { rdmsr(IA32_PMC7) },
 			_ => {
-				info!("No CPU known to implement 4 or more GP PMCs!");
+				info!("No CPU known to implement 8 or more GP PMCs!");
 				//return;  //TODO: We want, at some point, return an error
 				0
 			},
